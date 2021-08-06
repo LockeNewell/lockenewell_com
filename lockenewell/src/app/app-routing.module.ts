60ed69feca3b083116f54a257d53462036dae24c
Locke Newell <locke.newell@gmail.com>
2021-08-06 17:23:02 -0500

    update to angular 11

--- lockenewell/src/app/app-routing.module.ts
+++ lockenewell/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path:'portfolio', component: PortfolioComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
